cf8684e52b6481d2518568be7c3b942ab1691466
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-22 23:14:20 +0300

    Update useAuth.jsx

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,43 +1,34 @@
-import React, { useContext, useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import React, { useContext, useEffect } from "react"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+import { AuthContext } from "../context/AuthContext"
+
+
+
+
 
 function useAuth() {
-  const { setIsAuth, setOnLoad } = useContext(AuthContext);
+const { setIsAuth, setOnLoad } = useContext(AuthContext)
 
-  const queryFn = async () => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      throw new Error("No token found");
-    }
-    return await axios.get(`${import.meta.env.VITE_URL_BACKEND}/users/managers/auth`, {
-      headers: {
-        "Authorization": `Bearer ${token}`
-      },
-      withCredentials: true,
-    });
-  };
+const { isSuccess, isError } = useQuery({
+    queryKey:['auth'],
+    queryFn:async () => await axios.get(`${import.meta.env.VITE_URL_BACKEND}/users/managers/auth`,{withCredentials : true}),
+    refetchOnMount:false,
+    refetchOnWindowFocus:false,
+    refetchOnReconnect:false,
+    retry:false
+})
 
-  const { isSuccess, isError } = useQuery({
-    queryKey: ['auth'],
-    queryFn,
-    refetchOnMount: true,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-    retry: false,
-  });
+useEffect(() => {
+isSuccess && setIsAuth(true)
+isSuccess && setOnLoad(true)
+isError && setOnLoad(true)
+}, [isSuccess, isError])
 
-  useEffect(() => {
-    if (isSuccess) {
-      setIsAuth(true);
-    } else if (isError) {
-      setIsAuth(false);
-    }
-    setOnLoad(true);
-  }, [isSuccess, isError]);
-
-  return [isSuccess];
+return [ isSuccess ]
 }
 
-export default useAuth;
+
+export default useAuth
+
+
