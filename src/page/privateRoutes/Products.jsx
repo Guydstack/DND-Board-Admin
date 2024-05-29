e9d36fb766b304a6547b46213d32673a1d61a78d
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-29 20:09:58 +0300

    Update Products.jsx

--- src/page/privateRoutes/Products.jsx
+++ src/page/privateRoutes/Products.jsx
@@ -5,7 +5,6 @@ import {useQuery} from "@tanstack/react-query"
 import axios from "axios";
 import Pagination from "../../components/partials/products/Pagination";
 import { useState } from "react";
-// const url = "http://localhost:4000/products/managers/all";
 
 
 function Products() {
@@ -16,23 +15,13 @@ function Products() {
   const url = `${import.meta.env.VITE_URL_BACKEND}/products/managers/all?page=${page}&limit=${limit}`;
     //Only fro get Req
     const { data, isLoading, error, isError } = useQuery({
-      // queryKey:['get_products'],
       queryKey:['get_products', page],
       queryFn:async() => await axios.get(url),
-      // select:(res) => res.data.products,
       select:(res) => res.data,
       staleTime:1000 * 60,
       // gcTime:1000 * 60 * 60 * 24
     });
 
-
-  //Pagination client
-  // const [currentPage,setCurrentPage] = useState(1);
-  // const [productPerPage] = useState(3);
-
-  // const indexOfLastProduct = currentPage * productPerPage;
-  // const indexOfFirstProduct = indexOfLastProduct - productPerPage;
-  // const currentProducts = data?.slice(indexOfFirstProduct,indexOfLastProduct);
 
   return (
     <Container maxW={'container.xl'}>
@@ -40,14 +29,7 @@ function Products() {
       {isLoading && <Spinner /> }
       {isError && <span>{error.message}</span>}
       {!data && <span>No products</span>}
-      {/* {data && <ProductsTable products={currentProducts} />} */}
       {data && <ProductsTable products={data.products} />}
-      {/* {data && <Pagination
-      productPerPage={productPerPage}
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-      totalProducts={data.length} 
-      />} */}
       {data && <Pagination setPage={setPage} pages={data.pages} currentPage={page} />}
  
     </Container>
