c172e1ec91913c639949cdc8119d6ce24494a25d
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-29 20:17:13 +0300

    Update UsersTable.jsx

--- src/components/partials/users/UsersTable.jsx
+++ src/components/partials/users/UsersTable.jsx
@@ -32,23 +32,6 @@ import {
   }, [users]);
 
    
-//   const handleDelete = async (userId) => {
-    
-//     try {
-//         // // Step 1: Remove user from Realtime Database (rtdb)
-//         // await remove(ref(rtdb, `users/${userId}`));
-//         // console.log('User data deleted from Realtime Database successfully!');
-
-//         // // Step 2: Delete user document from Firestore (db)
-//         // await deleteDoc(doc(reference , userId));
-//         // console.log('User document deleted from Firestore successfully!');
-
-//         // Update loadingUser state if needed
-//         setLoadingUser(true);
-//     } catch (error) {
-//         console.error('Error deleting user:', error);
-//     } 
-// };
 
     return (
       <TableContainer mt={10}>
@@ -70,23 +53,6 @@ import {
                 <Td>{user.user_phone}</Td>
                 <Td>
                   <ButtonGroup gap="4">
-                    {/* <Button
-                      colorScheme="whiteAlpha"
-                      type="button"
-                      title="Delete"
-                      onClick={() => handleDelete(user._id)}
-                      isLoading={loadingUser}
-                      loadingText="Deleting"
-                    >
-                      <DeleteIcon bgSize={25} color={"red.300"} />
-                    </Button> */}
-                    {/* <Button
-                      colorScheme="blackAlpha"
-                      type="button"
-                      title="Update"
-                    >
-                      <EditIcon bgSize={25} color={"blue.300"} />
-                    </Button> */}
                     <Button
                       type="button"
                       title={user.active ? 'online' : 'offline'}
