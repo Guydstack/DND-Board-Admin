73b91750f842c85bbd2d21bbef3a57c4aeec4cee
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-29 20:21:35 +0300

    Update ModalProduct.jsx

--- src/components/common/ModalProduct.jsx
+++ src/components/common/ModalProduct.jsx
@@ -8,40 +8,24 @@ import {
   } from '@chakra-ui/react';
   import FormProduct from '../forms/FormProduct';
 
-// function ModalProduct({ isOpen , onClose, id, values }) {
-
-//   return (
-//     <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-//         <ModalOverlay />
-//         <ModalContent>
-//           <ModalHeader textAlign={'center'}>Edit Product</ModalHeader>
-//           <ModalCloseButton />
-//           <ModalBody pb={6}>
-//             <FormProduct url={`http://localhost:4000/products/managers/update/${id}`} method="PUT" onClose={onClose} values={values}/>
-//           </ModalBody>
-//         </ModalContent>
-//       </Modal>
-//   )
-// }
-
-function ModalProduct({ isOpen, onClose, id }) {
-  const url = id
-    ? `${import.meta.env.VITE_URL_BACKEND}/products/managers/update/${id}`
-    : `${import.meta.env.VITE_URL_BACKEND}/products/managers/add`;
-  const method = id ? "PUT" : 'POST';
-
-  return (
-    <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader textAlign={"center"}>{id ? 'Edit Product' : 'Add Product'}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody pb={6}>
-          <FormProduct url={url} method={method} onClose={onClose} />
-        </ModalBody>
-      </ModalContent>
-    </Modal>
-  );
-}
+    function ModalProduct({ isOpen, onClose, id }) {
+      const url = id
+        ? `${import.meta.env.VITE_URL_BACKEND}/products/managers/update/${id}`
+        : `${import.meta.env.VITE_URL_BACKEND}/products/managers/add`;
+      const method = id ? "PUT" : 'POST';
+    
+      return (
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader textAlign={"center"}>{id ? 'Edit Product' : 'Add Product'}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormProduct url={url} method={method} onClose={onClose} />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      );
+    }
 
 export default ModalProduct
