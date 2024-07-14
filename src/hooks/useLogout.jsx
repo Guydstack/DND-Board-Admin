a3c0aa9a30f8e0ac3aba9bebb0203cbd050bf08e
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-07-14 23:31:57 +0300

    Update useLogout.jsx

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,36 +1,3 @@
-// import { useMutation } from "@tanstack/react-query";
-// import axios from "axios";
-// import { useContext } from "react";
-// import { AuthContext } from "../context/AuthContext";
-// import { Toast } from "../lib";
-
-
-// function useLogout() {
-
-
-// const {setIsAuth , setUserProfile} = useContext(AuthContext)
-
-// const {mutate} = useMutation({
-//     mutationFn:async () => await axios.get(`${import.meta.env.VITE_URL_BACKEND}/users/managers/logout`,{withCredentials:true}),
-//     onSuccess:(res) => {
-//         setIsAuth(false)
-//         setUserProfile(null)
-//         Toast(res.data.message,true)
-//     },
-//     onError: (err) => {
-//         console.log(err)
-//         Toast(err.response.data.message,false)
-//     }
-// })    
-
-// return [ mutate ]
-// }
-
-// export default useLogout
-
-
-
-
 import { useMutation } from 'react-query';
 import axios from 'axios';
 import { useContext } from 'react';
