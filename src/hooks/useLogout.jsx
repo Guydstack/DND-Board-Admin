e322f8e69022483326188b8f495765de3f3fb059
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-07-14 23:48:29 +0300

    Update useLogout.jsx

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,15 +1,38 @@
-import { useMutation } from 'react-query';
-import axios from 'axios';
-import { useContext } from 'react';
-import AuthContext from '../context/AuthContext';
-import Toast from '../utils/Toast';
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Toast } from "../lib";
+
+
+
+
+// function useLogout() {
+// const {setIsAuth , setUserProfile} = useContext(AuthContext)
+
+// const {mutate} = useMutation({
+//     mutationFn:async () => await axios.get(`${import.meta.env.VITE_URL_BACKEND}/users/managers/logout`,{withCredentials:true}),
+//     onSuccess:(res) => {
+//         setIsAuth(false)
+//         setUserProfile(null)
+//         Toast(res.data.message,true)
+//     },
+//     onError: (err) => {
+//         console.log(err)
+//         Toast(err.response.data.message,false)
+//     }
+// })    
+// return [ mutate ]
+// }
+// export default useLogout
+
 
 const useLogout = () => {
   const { setIsAuth, setUserProfile } = useContext(AuthContext);
 
   const { mutate } = useMutation({
     mutationFn: async () => {
-      const token = localStorage.getItem('token')
+      const token = localStorage.getItem('token');
       return await axios.get('/users/managers/logout', {
         headers: {
           Authorization: `Bearer ${token}`
@@ -17,7 +40,7 @@ const useLogout = () => {
       });
     },
     onSuccess: (res) => {
-      localStorage.removeItem('token')
+      localStorage.removeItem('token');
       setIsAuth(false);
       setUserProfile(null);
       Toast(res.data.message, true);
