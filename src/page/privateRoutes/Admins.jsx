2fb09bf429aa25405ceb9434f5f37bb1481decd1
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-07-21 20:44:47 +0300

    Create Admins.jsx

---
+++ src/page/privateRoutes/Admins.jsx
@@ -0,0 +1,40 @@
+import { useQuery, useMutation ,useQueryClient} from "@tanstack/react-query";
+import axios from "axios";
+import { Container, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Toast } from "../../lib";
+import AdminsTable from "../../components/partials/admins/AdminsTable";
+
+const url = `${import.meta.env.VITE_URL_BACKEND}/users/managers/getall`;
+
+
+function Admins() {
+  //request for get all Mangers
+  const { isError, isLoading, data, error } = useQuery({
+    queryKey: ["get_admins"],
+    queryFn: async () => {
+        const response = await axios.get(url, {withCredentials: true});
+        return response.data; 
+      },
+    staleTime: 1000 * 60,
+  });
+
+  const managers = data?.managers ?? [];
+
+
+  return (
+    <Container maxW={"container.xl"}>
+      <Heading>Admins:</Heading>
+      {isLoading && <Spinner size={40} color="blue.500" />}
+      {isError && <span>{error.message}</span>}
+      {!isLoading && !isError && managers.length === 0 && <span>No Admins</span>}
+      {managers.length > 0 && (
+        <>
+          <Text>Total admins: {managers.length}</Text>
+          <AdminsTable admins={managers} />
+        </>
+      )}
+    </Container>
+  );
+}
+
+export default Admins;
